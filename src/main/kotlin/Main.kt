3ea61a92cf31db2c9d306ad404ddc43eff0c0065
Denis Aksenov <BLIHOK@mail.ru>
2023-12-30 10:51:06 +0300

    Test added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,15 +7,15 @@ import javax.xml.stream.events.Comment
 
 fun main() {
 
-//    val note= Notes<Note, Int>(storage = mutableListOf(), deletedStorage = mutableListOf())
-//    note.create(Note("1st note", 0, 2023,"My first note", 0, false))
-////    println(note.read())
-//    note.update(Note("3 note", 1, 2023,"My first note", 0, false))
-////    println(note.read())
-////    note.delete()
-////    println(note.read())
-//    println(note.getById(0))
-//    println(note.getFriendsNotes(0))
+    val note= Notes<Note, Int>(storage = mutableListOf(), deletedStorage = mutableListOf())
+    note.create(Note("1st note", 0, 2023,"My first note", 0, false))
+//    println(note.read())
+    note.update(Note("3 note", 1, 2023,"My first note", 0, false))
+//    println(note.read())
+//    note.delete()
+//    println(note.read())
+    println(note.getById(0))
+    println(note.getFriendsNotes(0))
 
     val comment = NoteComment<CommentNote, Int>(mutableListOf(), mutableListOf())
     comment.create(CommentNote("1st comment", 0, 2023,"My first comment", 0, false))
@@ -24,6 +24,7 @@ fun main() {
     println(comment.read())
     comment.restoreComment()
     println(comment.read())
+    comment.getComments(1)
 
 
 }
